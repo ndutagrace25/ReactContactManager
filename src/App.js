ab944de4ed6906016c6b36c4aff7c2aa925a4d15
Grace Nduta <45939062+ndutagrace25@users.noreply.github.com>
2019-07-06 13:38:55 +0300

    Changed BrowserRouter to HashRouter so as I can deplay on gitHub pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "@fortawesome/fontawesome-free/css/all.css";
 
